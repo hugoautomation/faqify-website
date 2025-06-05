dbe62e1c1f32cf76561398d792c1237ebd19c5ad
serge_0v <serge@schemaui.com>
2025-06-04 23:01:23 -0400

    feat: monorepo

---
+++ studio/schemas/blocks/feature/feature1/feature-image.ts
@@ -0,0 +1,21 @@
+import { defineField, defineType } from "sanity";
+import { Image } from "lucide-react";
+import image from "@/schemas/blocks/shared/image";
+
+export default defineType({
+  name: "feature-image",
+  type: "object",
+  icon: Image,
+  description: "Column with full image.",
+  fields: [image],
+  preview: {
+    select: {
+      title: "image.alt",
+    },
+    prepare({ title }) {
+      return {
+        title: title || "No Title",
+      };
+    },
+  },
+});
